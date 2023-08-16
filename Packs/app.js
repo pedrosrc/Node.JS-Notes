08e84db6737f545bbc80f886188dd1be83ede73c
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-08-16 09:30:07 -0300

    Promisses Async

--- Packs/app.js
+++ Packs/app.js
@@ -1,1 +1,21 @@
-console.log('Hello People')
+const {readFile, writeFile} = require('fs').promises
+//const util = require('util')
+//const readFilePromise = util.promisify(readFile)
+//const writeFilePromise = util.promisify(writeFile)
+
+
+const start = async() => {
+    try{
+        const first = await readFile('../Paste/first.txt', 'utf-8');
+        const second = await readFile('../Paste/second.txt', 'utf-8');
+        await writeFile(
+            '../Paste/result-text.txt',
+            `This is awesome: ${first} and ${second}`
+        )
+        console.log(first, second)
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+start()
